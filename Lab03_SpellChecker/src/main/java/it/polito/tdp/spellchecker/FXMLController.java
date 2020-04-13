cebbe895534a2d3a232ff5ab2352d806fa194acb
Maco <Maco@LAPTOP-O1JLL8OB>
2020-04-13 13:16:33 +0200

    Interfaccia completa

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,74 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Label lblChooseTheLanguage;
+
+    @FXML
+    private ComboBox<String> boxLanguages;
+
+    @FXML
+    private TextArea txtWords;
+
+    @FXML
+    private Button btnSpellCheck;
+
+    @FXML
+    private Label lblWrongWords;
+
+    @FXML
+    private TextArea txtWrongWords;
+
+    @FXML
+    private Label lblErrors;
+
+    @FXML
+    private Button btnClearText;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+    	txtWords.clear();
+    	txtWrongWords.clear();
+    	lblErrors.setText("Numero di Errori: ");
+    	lblTime.setText("Tempo di elaborazione: ");
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert lblChooseTheLanguage != null : "fx:id=\"lblChooseTheLanguage\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert boxLanguages != null : "fx:id=\"boxLanguages\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtWords != null : "fx:id=\"txtWords\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblWrongWords != null : "fx:id=\"lblWrongWords\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtWrongWords != null : "fx:id=\"txtWrongWords\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblErrors != null : "fx:id=\"lblErrors\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblTime != null : "fx:id=\"lblTime\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
-
-
